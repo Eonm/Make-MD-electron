c559383463aca76f139cc5a3e5dceea248e13ceb
eonm <eon.mathis@gmail.com>
2019-02-07 22:07:29 +0100

    Improve perf

--- scripts/allow-configuration.js
+++ scripts/allow-configuration.js
@@ -11,15 +11,20 @@ function disableConfiguration() {
   document.getElementById("create-doc").style.display = "none";
   Array.from(document.getElementsByClassName("configure")).forEach((elem) => {
     elem.classList.add("disabled");
-    document.getElementById("menu-open-or-create").click();
   })
+  document.getElementById("menu-open-or-create").click();
 }
 
 document.getElementById("menu-open-or-create").click();
 ipcRenderer.on("env-data", (event, envData) => {
   if (envData) {
     allowConfiguration();
+    document.getElementById('env-file').style.display = "block"
   } else {
     disableConfiguration();
   }
+})
+
+ipcRenderer.on('working-dir', (event, path) => {
+  document.getElementById('env-file').style.display = "none"
 })
