dc7a3d7ba380a7b2dc71fedefd6331e23f736638
eonm <eon.mathis@gmail.com>
2019-02-11 00:37:24 +0100

    Fix typo

--- background/load-rc-file.js
+++ background/load-rc-file.js
@@ -7,7 +7,7 @@ const {ipcMain, app} = require('electron');
 const { readLocalFile, parseRawEnvData } = require("./load-env-file.js")
 const { installRcFile } = require("../init/install-rc-file.js")
 
-const rcDataPath = `${app.getPath("appData")}/MakeMD/make-md.rc`
+const rcDataPath = `${app.getPath("appData")}/MakeMD/make-md.rc`;
 
 function loadRcData() {
   return readLocalFile(rcDataPath).then((rcData) => parseRawEnvData(rcData)).catch((error) => {
